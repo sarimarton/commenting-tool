f74f5ed4cdd31684c34fa7352e0320fcb3be0d60
sarimarton <sarimarton@gmail.com>
2021-02-17 07:18:32 +0100

    Focus textarea when it appears

--- src/simple/CommentForm.tsx
+++ src/simple/CommentForm.tsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useLayoutEffect, useRef, useState } from 'react'
 
 interface CommentFormProps {
   onSubmit: (text: string) => void
 }
 
 export default function CommentForm(props: CommentFormProps) {
+  // Focus the textarea when it appears
+  const ref = useRef<HTMLTextAreaElement>(null)
+  useLayoutEffect(() => {
+    // We can assume that the textarea exists
+    ref.current!.focus()
+  }, [])
+
   const [text, setText] = useState('')
 
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -18,7 +25,7 @@ export default function CommentForm(props: CommentFormProps) {
 
   return (
     <>
-      <textarea value={text} onChange={handleChange}></textarea>
+      <textarea ref={ref} value={text} onChange={handleChange}></textarea>
       <br />
       <button onClick={handleClick}>Add Comment</button>
     </>
