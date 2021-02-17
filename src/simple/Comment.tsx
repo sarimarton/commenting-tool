bb0dfa75239645e22d588148c92de37550e3205a
sarimarton <sarimarton@gmail.com>
2021-02-17 08:55:17 +0100

    Add yellow splash animation to new comments

--- src/simple/Comment.tsx
+++ src/simple/Comment.tsx
@@ -22,7 +22,10 @@ export default function Comment(props: CommentProps) {
   }
 
   return (
-    <div className="pt-2 pb-2">
+    // 'animate-splash' is a bit of a cheat here; I'm just making use of the
+    // fact that we start with an empty page, and due to a proper
+    // react-reconciliation, already displayed comments won't rerender.
+    <div className="pt-2 pb-2 animate-splash">
       <div>{props.comment.text}</div>
       <button onClick={handleReply} className="text-blue-500">
         Reply
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        splash: 'splash 3s normal forwards ease-out'
+      },
+      keyframes: {
+        splash: {
+          '0%': { 'background-color': colors.yellow['200'] },
+          '100%': { 'background-color': 'transparent' }
+        }
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
